3128605f0112cb847122eef4bc142319299a1662
Ethan Voth <ethanvoth1@gmail.com>
2024-03-06 04:57:17 -0500

    Draggable.svelte

--- editor/src/ui/panes/components/ComponentsSection.svelte
+++ editor/src/ui/panes/components/ComponentsSection.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-  import Portal from "svelte-portal";
   import { Action } from "../../../data/action";
   import { Manager } from "../../../data/manager";
   import { RepeatProps } from "../../../data/repeat";
   import { Component, LayerMode, Sequence } from "../../../data/sequence";
   import { project } from "../../../data/stores";
   import { Timestamp } from "../../../data/timestamp";
-  import ComponentBody from "../../timeline/ComponentBody.svelte";
+  import Draggable from "../../timeline/Draggable.svelte";
   import PaneSection from "../PaneSection.svelte";
 
   export let title: string;
@@ -14,19 +13,7 @@
   export let manager: Manager<Action | Sequence>;
   export let timelineElement: HTMLElement;
 
-  let dragging: Component | undefined = undefined;
-
-  // TODO: Error handling
-  $: draggingDuration = dragging?.getDuration()[1] ?? 5;
-
-  let draggingBox: DOMRect;
-  let dragOffset: [x: number, y: number];
-
-  let outsideBounds = true;
-
-  function dragStart(event: MouseEvent, component: Action | Sequence) {
-    event.preventDefault();
-
+  function getComponent(component: Action | Sequence): Component | undefined {
     if ($project.openedSequence === undefined) return;
 
     const repeatProps = new RepeatProps(
@@ -43,89 +30,39 @@
     );
 
     if (component instanceof Action) {
-      dragging = new Component(
+      return new Component(
         component.newState(),
         repeatProps,
         LayerMode.Coincide,
         $project.openedSequence.rootTimestamp
       );
     } else if (component instanceof Sequence) {
-      dragging = new Component(
+      return new Component(
         component,
         repeatProps,
         LayerMode.Coincide,
         $project.openedSequence.rootTimestamp
       );
     }
-
-    dragOffset = [event.offsetX, event.offsetY];
-
-    dragMove(event);
-  }
-
-  function dragMove(event: MouseEvent) {
-    const timelineBox = timelineElement.getBoundingClientRect();
-
-    let width = draggingDuration * $project.openedSequence!.scale;
-    let height = $project.openedSequence!.layerHeight;
-
-    draggingBox = new DOMRect(
-      event.clientX - Math.min(width, dragOffset[0]),
-      event.clientY - Math.min(height, dragOffset[1]),
-      width,
-      height
-    );
-
-    if (
-      event.clientX < timelineBox.left ||
-      event.clientX > timelineBox.right ||
-      event.clientY < timelineBox.top ||
-      event.clientY > timelineBox.bottom
-    ) {
-      outsideBounds = true;
-      return;
-    }
-    outsideBounds = false;
-  }
-
-  function dragEnd(event: MouseEvent) {
-    dragging = undefined;
   }
 </script>
 
 <PaneSection {title} {name}>
   {#each manager.children.entries() as [id, component] (id)}
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
     <!-- TODO: disallow any sequences which depend on current sequence -->
     {#if component !== $project.openedSequence}
-      <div
-        class="component"
-        on:mousedown={(event) => dragStart(event, component)}
-        style:background-color={`hsl(${component.hue}deg var(--component-saturation) var(--component-lightness))`}
-        class:disabled={$project.openedSequence === undefined}
-      >
-        {component.name}
-      </div>
+      <Draggable {timelineElement} getComponent={() => getComponent(component)}>
+        <div
+          class="component"
+          style:background-color={`hsl(${component.hue}deg var(--component-saturation) var(--component-lightness))`}
+          class:disabled={$project.openedSequence === undefined}
+        >
+          {component.name}
+        </div>
+      </Draggable>
     {/if}
   {/each}
 </PaneSection>
-
-{#if dragging !== undefined}
-  <Portal target="body">
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <div class="dragArea" on:mousemove={dragMove} on:mouseup={dragEnd}>
-      <div
-        class="dragContainer"
-        style:left={`${draggingBox.left}px`}
-        style:top={`${draggingBox.top}px`}
-        style:width={`${draggingBox.width}px`}
-        style:height={`${draggingBox.height}px`}
-      >
-        <ComponentBody component={dragging} disabled={outsideBounds} shadow />
-      </div>
-    </div>
-  </Portal>
-{/if}
 
 <style>
   .component {
@@ -137,17 +74,5 @@
 
   .disabled {
     cursor: not-allowed;
-  }
-
-  .dragArea {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-  }
-
-  .dragContainer {
-    position: absolute;
   }
 </style>
---
+++ editor/src/ui/timeline/Draggable.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import Portal from "svelte-portal";
+  import { Component } from "../../data/sequence";
+  import { project } from "../../data/stores";
+  import ComponentBody from "./ComponentBody.svelte";
+
+  export let timelineElement: HTMLElement;
+  export let getComponent: () => Component | undefined;
+  export let hideChildOnDrag = false;
+
+  let dragging: Component | undefined = undefined;
+
+  let draggingBox: DOMRect;
+  let dragOffset: [x: number, y: number];
+  let draggingDuration: number;
+
+  let outsideBounds = true;
+
+  function dragStart(event: MouseEvent) {
+    event.preventDefault();
+
+    dragging = getComponent();
+
+    // TODO: Error handling
+    if (dragging === undefined) return;
+
+    draggingDuration = dragging?.getDuration()[1] ?? 5;
+    dragOffset = [event.offsetX, event.offsetY];
+    dragMove(event);
+  }
+
+  function dragMove(event: MouseEvent) {
+    const timelineBox = timelineElement.getBoundingClientRect();
+
+    let width = draggingDuration * $project.openedSequence!.scale;
+    let height = $project.openedSequence!.layerHeight;
+
+    draggingBox = new DOMRect(
+      event.clientX - Math.min(width, dragOffset[0]),
+      event.clientY - Math.min(height, dragOffset[1]),
+      width,
+      height
+    );
+
+    if (
+      event.clientX < timelineBox.left ||
+      event.clientX > timelineBox.right ||
+      event.clientY < timelineBox.top ||
+      event.clientY > timelineBox.bottom
+    ) {
+      outsideBounds = true;
+      return;
+    }
+    outsideBounds = false;
+  }
+
+  function dragEnd(event: MouseEvent) {
+    dragging = undefined;
+  }
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div on:mousedown={(event) => dragStart(event)}>
+  {#if !(hideChildOnDrag && dragging !== undefined)}
+    <slot />
+  {/if}
+</div>
+
+{#if dragging !== undefined}
+  <Portal target="body">
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div class="dragArea" on:mousemove={dragMove} on:mouseup={dragEnd}>
+      <div
+        class="dragContainer"
+        style:left={`${draggingBox.left}px`}
+        style:top={`${draggingBox.top}px`}
+        style:width={`${draggingBox.width}px`}
+        style:height={`${draggingBox.height}px`}
+      >
+        <ComponentBody component={dragging} disabled={outsideBounds} shadow />
+      </div>
+    </div>
+  </Portal>
+{/if}
+
+<style>
+  .dragArea {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+  }
+
+  .dragContainer {
+    position: absolute;
+  }
+</style>
